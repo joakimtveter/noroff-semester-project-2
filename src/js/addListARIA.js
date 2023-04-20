6ea77c0408c04c44384d49ba6dbc87a02aff9e01
Joakim Tveter <41725646+joakimtveter@users.noreply.github.com>
2023-04-20 16:18:33 +0200

    Update addListARIA.js

--- src/js/addListARIA.js
+++ src/js/addListARIA.js
@@ -1,23 +1,23 @@
 function addListARIA() {
   try {
-    var allLists = document.querySelectorAll("ol, ul");
-    for (var i = 0; i < allLists.length; i++) {
+    const allLists = document.querySelectorAll("ol, ul");
+    for (let i = 0; i < allLists.length; i++) {
       allLists[i].setAttribute("role", "list");
     }
-    var allListItems = document.querySelectorAll("li");
-    for (var i = 0; i < allListItems.length; i++) {
+    const allListItems = document.querySelectorAll("li");
+    for (let i = 0; i < allListItems.length; i++) {
       allListItems[i].setAttribute("role", "listitem");
     }
-    var allDefLists = document.querySelectorAll("dl");
-    for (var i = 0; i < allDefLists.length; i++) {
+    const allDefLists = document.querySelectorAll("dl");
+    for (let i = 0; i < allDefLists.length; i++) {
       allDefLists[i].setAttribute("role", "associationlist list");
     }
-    var allDefTerms = document.querySelectorAll("dt");
-    for (var i = 0; i < allDefTerms.length; i++) {
+    const allDefTerms = document.querySelectorAll("dt");
+    for (let i = 0; i < allDefTerms.length; i++) {
       allDefTerms[i].setAttribute("role", "associationlistitemkey listitem");
     }
-    var allDefItems = document.querySelectorAll("dd");
-    for (var i = 0; i < allDefItems.length; i++) {
+    const allDefItems = document.querySelectorAll("dd");
+    for (let i = 0; i < allDefItems.length; i++) {
       allDefItems[i].setAttribute("role", "associationlistitemvalue listitem");
     }
   } catch (e) {
