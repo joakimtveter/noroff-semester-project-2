edb87c86c76026f02e97bc3950cd7d9729b1a892
Joakim Tveter <joakim@tveter.net>
2023-04-27 23:20:39 +0200

    Added submitt bid

---
+++ src/js/api/listings/submittBid.js
@@ -0,0 +1,18 @@
+import { post } from '../post';
+import '../../types';
+
+/**
+ * Submits a bid to a listing.
+ * @param {string} id - Listing ID
+ * @param {number} amount - Bid amount
+ */
+async function submittBid(id, amount) {
+    if (!id) throw new Error('No ID provided to submittBid()');
+    if (!amount) throw new Error('No amount provided to submittBid()');
+    if (amount < 0) throw new Error('Bid amount cannot be negative');
+    const newAmount = parseInt(amount);
+    if (typeof newAmount !== 'number') throw new Error('Bid amount must be a number');
+    return await post(`/listings/${id}/bids`, { amount: newAmount });  
+}
+
+export { submittBid }
