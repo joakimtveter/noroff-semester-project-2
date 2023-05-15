d66286c8172240ed03326b998e5f5ec5d0cad2d2
Joakim Tveter <joakim.tveter@netpower.no>
2023-05-15 23:07:51 +0200

    Added validation on login form

--- src/js/pages/auth/login.js
+++ src/js/pages/auth/login.js
@@ -1,15 +1,22 @@
 import { login } from '../../api/auth/login';
-import { isLoggedIn, redirect } from '../../utils.js';
+import { isLoggedIn, redirect, showToast } from '../../utils.js';
+import { validateLoginForm, handleFormErrors, clearFormErrors } from '../../validation';
 
 if (isLoggedIn()) redirect('/index.html');
 
 const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
 
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
     const formData = new FormData(loginForm);
     const email = formData.get('email');
     const password = formData.get('password');
 
-    login(email, password);
+    const errors = validateLoginForm(email, password);
+    if (errors.length > 0) {
+        handleFormErrors(e.target, errors);
+    } else {
+        clearFormErrors(e.target);
+        await login(email, password);
+    }
 });
