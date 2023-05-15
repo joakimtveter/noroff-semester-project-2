e5df2c4a84f707d1a41037a197c85cd7a3e335ac
Joakim Tveter <joakim.tveter@netpower.no>
2023-05-15 23:06:17 +0200

    Fixed default sort

--- src/js/pages/listings/index.js
+++ src/js/pages/listings/index.js
@@ -3,7 +3,7 @@ import { renderListingCard } from '../../render';
 
 const allListingsContainer = document.getElementById('all-listings');
 
-const listings = await getListings({ bids: true, active: true });
+const listings = await getListings({ sort: 'created', bids: true, active: true });
 
 listings.forEach((listing) => {
     const card = renderListingCard(listing);
